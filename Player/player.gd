eec2cf1f57adf4255a0e5c015d96f19c719c3a92
Marcel <mvelasquez2106@gmail.com>
2025-12-01 06:52:47 +0100

    Journal

--- Player/player.gd
+++ Player/player.gd
@@ -2,8 +2,13 @@ class_name Player
 extends CharacterBody2D
 
 @export var speed = 250
+@onready var inventory := $Journal
 
 func _physics_process(_delta: float) -> void:
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
 	move_and_slide()
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_released("inventory"):
+		inventory.visible = !inventory.visible
